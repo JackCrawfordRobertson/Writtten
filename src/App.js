14f1f95fb6312abcc04f0739e6c3d2b05082f621
Jack Robetson <jackrobertsonps3@gmail.com>
2023-12-18 19:37:19 +0000

    small updates again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import {TreeMap, TreeMapSeries, TreeMapRect} from "reaviz";
+import React, { useState, useEffect } from "react";
+import { TreeMap, TreeMapSeries, TreeMapRect } from "reaviz";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -10,29 +10,30 @@ import data from "./Data.json"; // Import the JSON data
 import Logo from "./logo.svg"; // Make sure the path is correct
 
 const App = () => {
-    const [ open, setOpen ] = useState(false);
+    const [open, setOpen] = useState(false);
     const [selectedNode, setSelectedNode] = useState(null);
-    const [ setIsMobile] = useState(false);
-
+    
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         const userAgent = typeof window.navigator === "undefined" ? "" : navigator.userAgent;
         const mobile = Boolean(userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i));
         setIsMobile(mobile);
-    }, [setIsMobile]);
+    }, []); // Empty dependency array, no need to include setIsMobile
 
-
+    // eslint-disable-next-line
     const handleClick = (event, data) => {
         setSelectedNode(data);
         setOpen(true);
     };
 
+
     return (
         <div className="App">
             <div className="App-header">
-                <h1 style={{marginBottom: "0.2em", textAlign: "left", display: "flex", alignItems: "center"}}>
+                <h1 style={{ marginBottom: "0.2em", textAlign: "left", display: "flex", alignItems: "center" }}>
                     Written Work
-                    <img src={Logo} alt="Logo" style={{width: "1em", marginLeft: "8px"}} />
+                    <img src={Logo} alt="Logo" style={{ width: "1em", marginLeft: "8px" }} />
                 </h1>
                 <p className="paragraph-text">
                     In this online archive, I'll visually estimate carbon emissions linked to stories, companies,
@@ -49,32 +50,32 @@ const App = () => {
                     series={<TreeMapSeries rect={<TreeMapRect onClick={handleClick} />} />}
                 />
                 <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth="md">
-                    <DialogTitle style={{paddingBottom: "1em"}}>
+                    <DialogTitle style={{ paddingBottom: "1em" }}>
                         {selectedNode ? selectedNode.data.key : "Loading..."}
                     </DialogTitle>
-                    <DialogContent style={{minWidth: "40vw", minHeight: "30vh", paddingTop: "0"}}>
+                    <DialogContent style={{ minWidth: "40vw", minHeight: "30vh", paddingTop: "0" }}>
                         {selectedNode && (
                             <>
                                 <div className="image-grid">
                                     {selectedNode.data.imageUrl
-                                    .split(",")
-                                    .map(
-                                        (url, index) =>
-                                            url.trim() && (
-                                                <img
-                                                    key={index}
-                                                    src={url.trim()}
-                                                    alt={`${index}`}
-                                                    className={`grid-item grid-item-${index}`}
-                                                />
-                                            )
-                                    )}
+                                        .split(",")
+                                        .map(
+                                            (url, index) =>
+                                                url.trim() && (
+                                                    <img
+                                                        key={index}
+                                                        src={url.trim()}
+                                                        alt={`${index}`}
+                                                        className={`grid-item grid-item-${index}`}
+                                                    />
+                                                )
+                                        )}
                                 </div>
                                 <p
                                     dangerouslySetInnerHTML={{
                                         __html: selectedNode.data.description.replace(/\n/g, "<br>"),
                                     }}
-                                    style={{marginBottom: "16px"}}
+                                    style={{ marginBottom: "16px" }}
                                 />
                             </>
                         )}
